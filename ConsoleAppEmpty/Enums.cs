ecb8bb5039a368beafa755df8a4655c62ff607d1
travis.chapman <travis.chapman@ENGRCKCP5S3>
2026-01-13 14:25:01 -0700

    Part 1 Complete

---
+++ ConsoleAppEmpty/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Practice
+{
+    public enum GameDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    public enum CharacterClass
+    {
+        Warrior,
+        Mage,
+        Rogue
+    }
+}
--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -2,9 +2,7 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
-        Console.WriteLine("Hello World");
-        Console.WriteLine("Hello World");
-        Console.WriteLine("Hello World");
+
     }
+
 }
